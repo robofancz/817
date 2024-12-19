089590106cadfd864a581807015bf9956c6d890a
robofancz <enduro@localhost>
2024-12-19 22:35:32 +0100

    a

---
+++ Main.js
@@ -0,0 +1,33 @@
+function home() {
+    window.location.href='Main.html'
+}
+function store (){
+    window.location.href='Store.html'
+}
+function employment (){
+    window.location.href='Employment.html'
+}
+function contacts (){
+    window.location.href='Contacts.html'
+}
+
+function toggle (){
+    toggle = false
+    toggle.onclick = true
+    if (toggle = true)
+        document.body.style.backgroundColor = black
+    else
+        document.body.style.backgroundColor = white
+    window.onclick.style.backgroundColor = black
+}
+function light_mode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+
+
+
+
+
+
